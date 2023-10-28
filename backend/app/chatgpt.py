42342e252c7c2fd8160232d94ecd8ea5c9e5c6a1
kino-ma <ma@kino.ma>
2023-10-29 00:39:08 +0900

    refactor: split the functio to check if trigerred

--- backend/app/chatgpt.py
+++ backend/app/chatgpt.py
@@ -1,10 +1,10 @@
 import openai
 import json
-from .googlecal.write_calendar import add_event_to_calendar 
+from .googlecal.write_calendar import add_event_to_calendar
 
-GPT_MODEL="gpt-4"
+GPT_MODEL = "gpt-4"
 
-DEFAULT_SYSTEM_PROMPT= """
+DEFAULT_SYSTEM_PROMPT = """
 あなたには，私の秘書として予定の管理業務に当たってもらいます．
 メッセージとして，取引先の方から，MTGの日程調整等の連絡が来ますので，それに対して，別途与えた私のスケジュールをもとに，相手の要望の条件に沿ったスケジュール候補を可能な限り多く選択し，教えてください．
 以下の点に注意してレスポンスを行なってください．
@@ -33,74 +33,76 @@ my_functions = [
             "type": "object",
             "properties": {
                 "summary": {
-                    "type":"string",
-                    "description":"ミーティングなどのアポイント予定を確保した際，保存するためのイベントのタイトル",
+                    "type": "string",
+                    "description": "ミーティングなどのアポイント予定を確保した際，保存するためのイベントのタイトル",
                 },
                 "location": {
-                    "type":"string",
-                    "description":"ミーティングなどのアポイント予定を確保した際，開催するための場所",
+                    "type": "string",
+                    "description": "ミーティングなどのアポイント予定を確保した際，開催するための場所",
                 },
                 "member": {
-                    "type":"string",
-                    "description" : "ミーティングなどのアポイント予定を確保した際，イベントの参加者. カンマ区切りで複数人登録可能",
+                    "type": "string",
+                    "description": "ミーティングなどのアポイント予定を確保した際，イベントの参加者. カンマ区切りで複数人登録可能",
                 },
                 "start": {
                     "type": "string",
-                    "description":"ミーティングなどのアポイント予定を確保した際, 予定の開始時刻．dateTimeの形式であり，timezoneはAsia/Tokyo",
+                    "description": "ミーティングなどのアポイント予定を確保した際, 予定の開始時刻．dateTimeの形式であり，timezoneはAsia/Tokyo",
                     "format": "date-time",
                 },
                 "end": {
                     "type": "string",
-                    "description":"ミーティングなどのアポイント予定を確保した際, 予定の終了時刻．dateTimeの形式であり，timezoneはAsia/Tokyo. 開始時刻のみの指定だった場合，1時間として確保",
+                    "description": "ミーティングなどのアポイント予定を確保した際, 予定の終了時刻．dateTimeの形式であり，timezoneはAsia/Tokyo. 開始時刻のみの指定だった場合，1時間として確保",
                     "format": "date-time",
                 },
             },
-            "required": ["summary", "start", "end"]
-        }
+            "required": ["summary", "start", "end"],
+        },
     }
 ]
 
-def create_event(summary, start, end, location=None, description=None, member=None, timeZone='Asia/Tokyo'):
+
+def create_event(
+    summary,
+    start,
+    end,
+    location=None,
+    description=None,
+    member=None,
+    timeZone="Asia/Tokyo",
+):
     event = {
-        'summary': summary,
-        'location': location,
-        'description': None if member is None else f"参加者: {member}",
-        'start': {
-            'dateTime': start,
-            'timeZone': timeZone,
+        "summary": summary,
+        "location": location,
+        "description": None if member is None else f"参加者: {member}",
+        "start": {
+            "dateTime": start,
+            "timeZone": timeZone,
         },
-        'end': {
-            'dateTime': end,
-            'timeZone': timeZone,
+        "end": {
+            "dateTime": end,
+            "timeZone": timeZone,
         },
     }
     return event
 
+
 def send_chat(
-    user_message: str,
-    system_message: str = DEFAULT_SYSTEM_PROMPT
+    user_message: str, system_message: str = DEFAULT_SYSTEM_PROMPT
 ) -> openai.ChatCompletion:
     res = openai.ChatCompletion.create(
         model=GPT_MODEL,
         functions=my_functions,
         function_call="auto",
         messages=[
-            {
-                "role": "system",
-                "content": system_message
-            },
-            {
-                "role": "user",
-                "content": user_message
-            },
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": user_message},
         ],
     )
 
     print("prompt:", system_message, user_message)
     print("Got response:", res)
 
-    message = res["choices"][0]["message"]
-    if message.get("function_call"):
+    if is_function_triggered(res):
         # 関数を使用すると判断された場合
 
         # 使うと判断された関数名
@@ -117,3 +119,7 @@ def send_chat(
 
     return res
 
+
+def is_function_triggered(chat_response: dict):
+    message = chat_response["choices"][0]["message"]
+    return message.get("function_call") is not None
