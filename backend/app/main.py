57cb99bb674a7f18fde1d3d5db19616744a970ab
kino-ma <ma@kino.ma>
2023-10-28 12:13:53 +0900

    feat: call chatgpt api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
+import openai
 from fastapi import FastAPI
 
-from . import schemas
+from . import schemas, chatgpt
 
 app = FastAPI()
+
+
+with open('.openai.key', 'r') as f:
+    openai.api_key = f.read()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/chat", response_model=schemas.Chat)
+def post_chat(chat: schemas.ChatPost):
+    res = chatgpt.send_chat(chat.message)
